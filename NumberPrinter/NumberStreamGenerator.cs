3b1e19d71f102fc42b1b1bf08b11b34d930f26da
YellowVulture <mckain.7@osu.edu>
2015-12-10 08:30:55 -0600

    Commit for NumberPrinter demo project.

---
+++ NumberPrinter/NumberStreamGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberPrinter
+{
+    /// <summary>
+    /// Encapsulates the process of producing a list of strings based on a provided key/value rule set.
+    /// </summary>
+    public class NumberStreamGenerator
+    {
+        private IDictionary<int, string> mappingRules;
+        private IOrderedEnumerable<int> keyValues;
+
+        /// <summary>
+        /// Creates an instance for a provided set of mapping rules.
+        /// </summary>
+        /// <remarks>
+        /// The output mappings will be applied with precedent given to the highest key value.
+        /// </remarks>
+        /// <param name="outputMappingRules">The key value pairs that define</param>
+        public NumberStreamGenerator(IDictionary<int, string> outputMappingRules)
+        {
+            this.mappingRules = outputMappingRules;
+            this.keyValues = (this.mappingRules != null) ? 
+                this.mappingRules.Keys.Where(x => x != 0).OrderByDescending(x => x) : 
+                Enumerable.Empty<int>().OrderByDescending(x => x);
+        }
+
+        /// <summary>
+        /// Produces a stream of string values from 1 to the provided upper bound based on the rule mappings provided to the object.
+        /// </summary>
+        /// <param name="upperBound">The highest value to generate a return value for.</param>
+        /// <returns>An enumerable of strings based on the rule mappings.</returns>
+        public IEnumerable<string> GenerateOutputStream(int upperBound)
+        {
+            for (int i = 1; i <= upperBound; i++)
+            {
+                yield return PrintNumber(i);
+            }
+            yield break;
+        }
+
+        private string PrintNumber(int number)
+        {
+            foreach (var modKey in keyValues)
+            {
+                if (number % modKey == 0) return this.mappingRules[modKey];
+            }
+
+            return number.ToString();
+        }
+    }
+}
---
+++ NumberPrinterTests/NumberPrinterTest.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+using NumberPrinter;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace NumberPrinterTests
+{
+    [TestFixture]
+    public class NumberPrinterTest
+    {
+        [Test]
+        [Description("Verify a simple sequence produced by the NumberPrinter.")]
+        public void NumberPrinter_Verify_Sequence()
+        {
+            var rules = new Dictionary<int, string>();
+            rules.Add(3, "Fizz");
+            rules.Add(5, "Buzz");
+
+            var generator = new NumberStreamGenerator(rules);
+            var list = generator.GenerateOutputStream(15).ToList();
+
+            CollectionAssert.AreEqual(new List<string>() { 
+                "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "Buzz"}, list);
+        }
+
+        [Test]
+        [Description("In the case where more than 1 key meet the criteria, the highest key value will be used.")]
+        public void NumberPrinter_GreaterPrecedent()
+        {
+            var rules = new Dictionary<int, string>();
+            rules.Add(3, "Fizz");
+            rules.Add(5, "Buzz");
+
+            var generator = new NumberStreamGenerator(rules);
+            var list = generator.GenerateOutputStream(15);
+            Assert.AreEqual(list.Last(), "Buzz");
+        }
+
+        [Test]
+        [Description("A null input can be provided to the object, in which case there are no rules to apply.")]
+        public void NumberPrinter_Null_Rules()
+        {
+            var generator = new NumberStreamGenerator(null);
+            var list = generator.GenerateOutputStream(15).ToList();
+
+            Assert.IsNotNull(list);
+            CollectionAssert.AreEqual(new List<string>() { 
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"}, list);
+        }
+
+        [Test]
+        [Description("An empty input can be provided to the object, in which case there are no rules to apply.")]
+        public void NumberPrinter_Empty_Rules()
+        {
+            var generator = new NumberStreamGenerator(new Dictionary<int, string>());
+            var list = generator.GenerateOutputStream(15).ToList();
+
+            Assert.IsNotNull(list);
+            CollectionAssert.AreEqual(new List<string>() { 
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"}, list);
+        }
+
+        [Test]
+        [Description("A rule with a key value of 0 cannot be processed because of division of 0.  Key values of 0 are ignored.")]
+        public void NumberPrinter_Zero_Rule()
+        {
+            var rules = new Dictionary<int, string>();
+            rules.Add(0, "Beep");
+
+            var generator = new NumberStreamGenerator(rules);
+            var list = generator.GenerateOutputStream(15).ToList();
+
+            Assert.IsNotNull(list);
+            CollectionAssert.AreEqual(new List<string>() { 
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"}, list);
+        }
+
+        [Test]
+        [Description("A value of null is a legitimate output, verify the output is correct for this case.")]
+        public void NumberPrinter_Null_Map_Value()
+        {
+            var rules = new Dictionary<int, string>();
+            rules.Add(3, null);
+
+            var generator = new NumberStreamGenerator(rules);
+            var list = generator.GenerateOutputStream(15).ToList();
+
+            Assert.IsNotNull(list);
+            CollectionAssert.AreEqual(new List<string>() { 
+            "1", "2", null, "4", "5", null, "7", "8", null, "10", "11", null, "13", "14", null}, list);
+        }
+
+        [Test]
+        [Description("A provided upper bound of 0 will produce no result.")]
+        public void NumberPrinter_Zero_Upper_Bound()
+        {
+            var rules = new Dictionary<int, string>();
+            rules.Add(3, "Fizz");
+            rules.Add(5, "Buzz");
+
+            var generator = new NumberStreamGenerator(rules);
+            var list = generator.GenerateOutputStream(0).ToList();
+
+            Assert.AreEqual(list.Count, 0);
+        }
+
+        [Test]
+        [Description("A provided upper bound less than 0 will produce no result.")]
+        public void NumberPrinter_Negative_Upper_Bound()
+        {
+            var rules = new Dictionary<int, string>();
+            rules.Add(3, "Fizz");
+            rules.Add(5, "Buzz");
+
+            var generator = new NumberStreamGenerator(rules);
+            var list = generator.GenerateOutputStream(-1).ToList();
+
+            Assert.AreEqual(list.Count, 0);
+        }
+
+        [Test]
+        [Description("Provided rules can technically be modified while the result is being process, verify the results behave accordingly.")]
+        public void NumberPrinter_Input_Modified()
+        {
+            var rules = new Dictionary<int, string>();
+            rules.Add(3, "Fizz");
+            rules.Add(5, "Buzz");
+
+            var generator = new NumberStreamGenerator(rules);
+            var list = generator.GenerateOutputStream(15);
+
+            rules.Add(7, "Burp");
+            rules.Remove(5);
+
+            CollectionAssert.AreEqual(new List<string>() 
+            { "1", "2", "Fizz", "4", "5", "Fizz", "Burp", "8", "Fizz", "10", "11", "Fizz", "13", "Burp", "Fizz" }, 
+            list.ToList());
+        }
+    }
+}
